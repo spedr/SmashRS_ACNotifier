cd9ba1839055d710c1a54f63af8036658f7a69ef
spedr <pedro.sanvido@acad.pucrs.br>
2017-12-12 20:27:14 -0200

    fixing bugs

--- mainbot.js
+++ mainbot.js
@@ -3,46 +3,34 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
+//Last user
+var lastUser;
+
 //Ready event
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
-//queryUserList event
-client.on("message", (message) => {
-  if (message.content.startsWith("queryUserList")) {
-    let guild = client.guilds.find("name", "SmashRS");
-    guild.members.forEach(function(value, key) {
-      //console.log(guild.members.get(key).user.username);
-      //console.log(guild.members.get(key).roles);]
-      let ac = guild.members.get(key).roles.find("name", "Animal Crossing");
-      if(ac!=null){
-        if(guild.members.get(key).user.presence.game==null){
-          client.channels.get('389933420270321664').send('O user ' + guild.members.get(key).user.toString() + ' tem role de Animal Crossing.');
-        }else if(guild.members.get(key).user.presence.game.name=="Dolphin")
-          client.channels.get('389933420270321664').send('O user ' + guild.members.get(key).user.toString() + ' tem role de Animal Crossing e está jogando Dolphin.');
-      }else{
-        client.channels.get('389933420270321664').send('O user ' + guild.members.get(key).user.toString() + ' não tem role de Animal Crossing.');
-      }
-      console.log(key + " = " + value);
-      ac = null;
-    }, guild.members)
-  }
-});
 
 //Main functionality
 client.on('presenceUpdate', (oldMember, newMember) => {
+  if(lastUser == newMember.user.username)
+    return;
+
+  var i = 0;
   if(newMember.presence.game!=null){
     if(newMember.presence.game.name=="Dolphin"){
         let guild = client.guilds.find("name", "SmashRS");
         guild.members.forEach(function(value, key) {
           //console.log(guild.members.get(key).user.username);
           //console.log(guild.members.get(key).roles);]
-          let ac = guild.members.get(key).roles.find("name", "Animal Crossing");
+          let ac = guild.members.get(key).roles.find("name", "villagers");
+          let ac2 = newMember.roles.find("name", "villagers");
           var currentMember = guild.members.get(key);
           if(currentMember.user.presence.game!=null){
-            if(ac!=null && currentMember.user.presence.game.name=="Dolphin" && currentMember.user.username!=newMember.user.username){
+            if(ac!=null && currentMember.user.presence.game.name=="Dolphin" && currentMember.user.username!=newMember.user.username && ac2!=null){
               client.channels.get('389933420270321664').send(newMember.user.toString() + ' o ' + currentMember.user.username + ' já está possivelmente jogando Animal Crossing! Cuidado!');
+              lastUser == newMember.user.username;
             }
           }
           ac = null;
